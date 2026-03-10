166b5c5ed7ffd99a730ba2c2be6057db77f448af
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-10 19:43:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import {config} from "dotenv"
+config();
+
+import { ChatGroq } from "@langchain/groq";
+import { PromptTemplate } from "@langchain/core/prompts";
+
+
+const template = PromptTemplate.fromTemplate(`explain {topic} in hinglish`);
+
+
+const model = new ChatGroq({
+    apiKey:process.env.GROQ_API_KEY,
+    model:"llama-3.1-8b-instant"
+})
+
+
+template.pipe(model).invoke({topic:"express.js"}).then((response)=>{console.log(response.content)})
