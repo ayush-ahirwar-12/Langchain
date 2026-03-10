b052c192a2b4f892167dafb1d53951250622df88
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-10 19:54:16 +0530

    changed api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {config} from "dotenv";
-config()
+config();
 
 import { ChatGroq } from "@langchain/groq";
 import { PromptTemplate } from "@langchain/core/prompts";
@@ -11,7 +11,7 @@ const template = PromptTemplate.fromTemplate(`explain {topic} in hinglish`);
 const model = new ChatGroq({
     apiKey:process.env.GROQ_API_KEY,
     model:"llama-3.1-8b-instant"
-})
+});
 
 
-template.pipe(model).invoke({topic:"express.js"}).then((response)=>{console.log(response.content)})
+template.pipe(model).invoke({topic:"express.js"}).then((response)=>{console.log(response.content)});
